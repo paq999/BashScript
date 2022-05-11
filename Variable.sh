fc89697fc3a6a80b652ca659c7d088ae2cec128e
paq999 <paq99@interia.pl>
2022-05-11 09:12:19 +0000

    Polecenie read

---
+++ Variable.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# echo "Podaj swoje Imie:"
+# read Imie
+# echo "Podaj swoje nazwisko:"
+# read nazwisko
+# echo "podaj numer tel. :"
+# read tel
+
+# echo "Informacje o uzyskowniku: imie $Imie nazwisko $nazwisko tel.: $tel"
+
+# exit 0
+
+# echo "podaj 3 parametry"
+# read x y
+
+# echo "Patarametr 1: $x"
+# echo "Patarametr 2: $y"
+
+# exit 0
+
+read -p "jaką lubisz potrawę: " potrawa
+echo $potrawa
